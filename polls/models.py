8a1e0c3bae6bc4df77a02613aa80a3d74d8fb4bb
m <m@gmail.com>
2021-04-02 14:06:47 +0800

    udur

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,6 @@
 import datetime
 from django.db import models
 from django.utils import timezone
-from polls.models import Choice, Question
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
